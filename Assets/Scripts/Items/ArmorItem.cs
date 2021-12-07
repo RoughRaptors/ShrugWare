85b5f58ae3dbd0948ecacbd1cd18ea9b92e137d3
RoughRaptors <RoughRaptors@Gmail.com>
2021-12-07 13:10:19 -0800

    Implemented set bonuses

--- Assets/Scripts/Items/ArmorItem.cs
+++ Assets/Scripts/Items/ArmorItem.cs
@@ -6,21 +6,32 @@ namespace ShrugWare
 {
     public class ArmorItem : Item
     {
-        public enum ArmorSlot
-        {
-            Head = 0,
-            Chest,
-            Gloves,
-            Legs,
-            Boots
-        }
+        protected DataManager.ArmorSlot armorSlot;
+        public DataManager.ArmorSlot GetArmorSlot() { return armorSlot; }
 
-        protected ArmorSlot armorSlot;
+        protected DataManager.ArmorSet armorSet;
+        public DataManager.ArmorSet GetArmorSet() { return armorSet; }
 
-        public ArmorItem(ArmorSlot slot)
+        private List<DataManager.StatEffect> setBonuses = new List<DataManager.StatEffect>();
+        public List<DataManager.StatEffect> GetSetBonuses() { return setBonuses; }
+
+        public ArmorItem(DataManager.ArmorSlot newArmorSlot, DataManager.ArmorSet newArmorSet)
         {
-            armorSlot = slot;
+            armorSlot = newArmorSlot;
+            armorSet = newArmorSet;
             itemType = DataManager.ItemType.Armor;
+
+            // hard code our set bonus for daunting infero right now
+            if(armorSet == DataManager.ArmorSet.DauntingInferno)
+            {
+                // +10% max hp
+                DataManager.StatEffect maxHPEffect;
+                maxHPEffect.amount = 10;
+                maxHPEffect.effectType = DataManager.StatModifierType.PlayerMaxHealth;
+                maxHPEffect.asPercentage = true;
+
+                setBonuses.Add(maxHPEffect);
+            }
         }
     }
 }
