cdf700e067a9649e9c5cc59e02ad491834c534b2
RoughRaptors <RoughRaptors@Gmail.com>
2022-11-19 11:45:27 -0800

    Various fixes to make the game presentable.

--- Assets/Scripts/OverworldUIManager.cs
+++ Assets/Scripts/OverworldUIManager.cs
@@ -27,29 +27,45 @@ namespace ShrugWare
                 Destroy(gameObject);
             }
 
-            Instance.SetCanvasEnabled(true);
+            Instance.SetCanvasEnabled(true);            
         }
 
         public void UpdateUI()
         {
+            if(OverworldManager.Instance == null || OverworldManager.Instance.CurLevel == null)
+            {
+                return;
+            }
+
             OverworldLevel curLevel = OverworldManager.Instance.GetOverworldLevel(OverworldManager.Instance.CurLevel.LevelID);
             if (curLevel != null)
             {
-                curLevelText.text = "Cur Level ID: " + curLevel.LevelID.ToString() +
-                    "\n" + "Scene ID: " + curLevel.SceneIDToLoad +
-                    "\n" + "Type : " + curLevel.LevelType.ToString();
+                if(curLevel.Completed)
+                {
+                    curLevelText.text = "Cur Level ID: " + curLevel.LevelID.ToString() +
+                        "\n" + "Scene ID: " + curLevel.SceneIDToLoad +
+                        "\n" + "**COMPLETED**";
+                }
+                else
+                {
+                    curLevelText.text = "Cur Level ID: " + curLevel.LevelID.ToString() +
+                        "\n" + "Scene ID: " + curLevel.SceneIDToLoad +
+                        "\n" + "Type : " + curLevel.LevelType.ToString();
+                }
             }
         }
 
         public void SetCanvasEnabled(bool enabled)
         {
             mainUICanvas.SetActive(enabled);
+            UpdateUI();
         }
 
         public void EnterCurLevelClicked()
         {
+            // don't go in completed boss levels (yet!)
             OverworldLevel overworldLevel = OverworldManager.Instance.GetOverworldLevel(OverworldManager.Instance.CurLevel.LevelID);
-            if (overworldLevel != null)
+            if (overworldLevel != null && !(overworldLevel.LevelType == DataManager.OverworldLevelType.Boss && overworldLevel.Completed))
             {
                 overworldLevel.EnterLevel();
             }
