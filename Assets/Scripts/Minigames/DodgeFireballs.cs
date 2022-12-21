0c6d6c6cd380d857a1f3c151b886a2d50de12c8a
RoughRaptors <RoughRaptors@Gmail.com>
2022-12-20 19:06:26 -0800

    Minigame spawning

--- Assets/Scripts/Minigames/DodgeFireballs.cs
+++ Assets/Scripts/Minigames/DodgeFireballs.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 namespace ShrugWare
 {
+    // 45 seconds long
+    // 0-15s - come from right
+    // 15s-30s come from top + right
+    // 30s-45s come from bottom + top + right
+
     public class DodgeFireballs : Minigame
     {
         [SerializeField]
         GameObject fireballInitObj;
 
-        const float FIREBALL_X_POS = 125;
+        const float FIREBALL_X_MIN = -10;
+        const float FIREBALL_X_MAX = 125;
         const float FIREBALL_Y_MIN = -30;
         const float FIREBALL_Y_MAX = 40;
         const float FIREBALL_SPEED_MIN = 0.05f;
@@ -18,12 +24,22 @@ namespace ShrugWare
         const float FIREBALL_SPAWN_INTERVAL = 0.5f;
         float timeSinceLastSpawn = 0.0f;
 
+        private float timeInGame = 0.0f;
+
+        private enum FromDirection
+        {
+            FromRight = 0,
+            FromTop,
+            FromBottom
+        }
+
         protected struct Fireball
         {
             public GameObject fireballObj;
             public Vector3 position;
             public Vector3 targetPos;
             public float speed;
+            FromDirection fromDir;
         }
 
         private List<Fireball> fireballsList = new List<Fireball>();
@@ -33,7 +49,7 @@ namespace ShrugWare
             // initial wave
             for(int i = 0; i < 15; ++i)
             {
-                SpawnFireball();
+                SpawnFireball(FromDirection.FromRight);
             }
         }
 
@@ -42,28 +58,75 @@ namespace ShrugWare
             if(timeSinceLastSpawn >= FIREBALL_SPAWN_INTERVAL)
             {
                 timeSinceLastSpawn = 0;
-                SpawnFireball();                
+
+                // cast down to int to be able to cleanly mod 15
+                // spawn more at 15s and 30s
+                if ((int)timeInGame != 0 && (int)timeInGame % 30 == 0)
+                {
+                    // spawn top
+                    for (int i = 0; i < 15; ++i)
+                    {
+                        SpawnFireball(FromDirection.FromBottom);
+                    }
+                }
+                else if ((int)timeInGame != 0 && (int)timeInGame % 15 == 0)
+                {
+                    // spawn bottom
+                    for (int i = 0; i < 15; ++i)
+                    {
+                        SpawnFireball(FromDirection.FromTop);
+                    }
+                }
+
+                SpawnFireball(FromDirection.FromRight);
+                SpawnFireball(FromDirection.FromBottom);
+                SpawnFireball(FromDirection.FromTop);
             }
 
             UpdateFireballs();
             timeSinceLastSpawn += Time.deltaTime;
+            timeInGame += Time.deltaTime;
         }
 
-        private void SpawnFireball()
+        private void SpawnFireball(FromDirection fromDir)
         {
             Fireball newFireball = new Fireball();
             GameObject newFireballObj = Instantiate(fireballInitObj);
             newFireball.fireballObj = newFireballObj;
             newFireballObj.gameObject.SetActive(true);
 
-            float yPos = UnityEngine.Random.Range(FIREBALL_Y_MIN, FIREBALL_Y_MAX);
-            newFireballObj.transform.position = new Vector3(FIREBALL_X_POS, yPos, 0);
-
             float speed = UnityEngine.Random.Range(FIREBALL_SPEED_MIN, FIREBALL_SPEED_MAX);
             newFireball.speed = speed;
 
-            Vector3 targetPos = new Vector3(-25, yPos, 0);
-            newFireball.targetPos = targetPos;
+            float xPos = 0.0f;
+            float yPos = 0.0f;
+            if (fromDir == FromDirection.FromRight)
+            {
+                xPos = FIREBALL_X_MAX;
+                yPos = UnityEngine.Random.Range(FIREBALL_Y_MIN, FIREBALL_Y_MAX);
+                newFireballObj.transform.position = new Vector3(xPos, yPos, 0);
+
+                Vector3 targetPos = new Vector3(-25, yPos, 0);
+                newFireball.targetPos = targetPos;
+            }
+            else if (fromDir == FromDirection.FromTop)
+            {
+                xPos = UnityEngine.Random.Range(FIREBALL_X_MIN, FIREBALL_X_MAX);
+                yPos = 50;
+                newFireballObj.transform.position = new Vector3(xPos, yPos, 0);
+
+                Vector3 targetPos = new Vector3(xPos, -40, 0);
+                newFireball.targetPos = targetPos;
+            }
+            else if (fromDir == FromDirection.FromBottom)
+            {
+                xPos = UnityEngine.Random.Range(FIREBALL_X_MIN, FIREBALL_X_MAX);
+                yPos = -40;
+                newFireballObj.transform.position = new Vector3(xPos, yPos, 0);
+
+                Vector3 targetPos = new Vector3(xPos, 50, 0);
+                newFireball.targetPos = targetPos;
+            }
 
             fireballsList.Add(newFireball);
         }
