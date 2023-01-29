d49f8ac7215b342712f6108b6ce42c6ffbd31868
RoughRaptors <RoughRaptors@Gmail.com>
2023-01-28 16:05:21 -0800

    Polish

--- Assets/Scripts/Various Other/MainMenu.cs
+++ Assets/Scripts/Various Other/MainMenu.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
 
 namespace ShrugWare
 {
     public class MainMenu : MonoBehaviour
     {
+        private void Awake()
+        {
+            // this was generating errors on dev build startup
+            // https://forum.unity.com/threads/errors-with-the-urp-debug-manager.987795/
+            DebugManager.instance.enableRuntimeUI = false;
+        }
+
         public void OnStartClicked()
         {
             SceneManager.LoadScene((int)DataManager.Scenes.OverworldScene);
