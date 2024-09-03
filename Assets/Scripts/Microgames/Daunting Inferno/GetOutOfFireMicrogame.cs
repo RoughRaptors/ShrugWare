5d27285f7c2566086610549308ed92aa25349724
RoughRaptors <RoughRaptors@Gmail.com>
2024-09-03 16:03:20 -0700

    Completely revamped the get out of fire microgame

--- Assets/Scripts/Microgames/Daunting Inferno/GetOutOfFireMicrogame.cs
+++ Assets/Scripts/Microgames/Daunting Inferno/GetOutOfFireMicrogame.cs
@@ -4,26 +4,73 @@ namespace ShrugWare
 {
     public class GetOutOfFireMicrogame : Microgame
     {
-        private bool inFire = true;
+        [SerializeField]
+        GameObject topWall;
+
+        [SerializeField]
+        GameObject bottomWall;
+
+        [SerializeField]
+        GameObject leftWall;
+
+        [SerializeField]
+        GameObject rightWall;
+
+        private bool escaped = false;
 
         protected override void OnEnable()
         {
             base.OnEnable();
-            PlayerCollider.OnBadExit += FireEscape;
+            PlayerCollider.OnGoodCollision += FireEscape;
+            PlayerCollider.OnBadCollision += HitFire;
         }
 
         protected override void OnDisable()
         {
             base.OnDisable();
-            PlayerCollider.OnBadExit -= FireEscape;
+            PlayerCollider.OnGoodCollision -= FireEscape;
+            PlayerCollider.OnBadCollision -= HitFire;
         }
 
-        protected override bool VictoryCheck() => !inFire;
+        protected override void OnMyGameStart()
+        {
+            base.OnMyGameStart();
+
+            int randNumber = UnityEngine.Random.Range(0, 4);
+            if(randNumber == 0)
+            {
+                topWall.SetActive(false);
+            }
+            else if (randNumber == 0)
+            {
+                bottomWall.SetActive(false);
+            }
+            else if (randNumber == 0)
+            {
+                leftWall.SetActive(false);
+            }
+            else
+            {
+                rightWall.SetActive(false);
+            }
+        }
+
+        protected override void OnMyGameTick(float timePercentLeft)
+        {
+            base.OnMyGameTick(timePercentLeft);
+        }
+
+        protected override bool VictoryCheck() => escaped;
+
+        private void HitFire(GameObject obj)
+        {
+            SetMicrogameEndText(false);
+        }
 
         // once they're out, we don't care if they go back in
         private void FireEscape(GameObject fireObject)
         {
-            inFire = false;
+            escaped = true;
             SetMicrogameEndText(true);
         }
     }
