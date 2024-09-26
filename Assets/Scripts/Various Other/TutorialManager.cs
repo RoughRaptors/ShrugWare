d48c7ea07bfbc6be378e5bf8d3fba76bcbeedeb7
RoughRaptors <roughraptors@gmail.com>
2024-09-26 15:09:35 -0700

    Tutorial pages (#224)
    * Implemented a second tutorial page
    
    * Changed tutorial text

--- Assets/Scripts/Various Other/TutorialManager.cs
+++ Assets/Scripts/Various Other/TutorialManager.cs
@@ -7,6 +7,12 @@ namespace ShrugWare
     {
         public static TutorialManager Instance;
 
+        [SerializeField]
+        GameObject page1;
+
+        [SerializeField]
+        GameObject page2;
+
         private void Awake()
         {
             if (Instance == null)
@@ -27,6 +33,18 @@ namespace ShrugWare
         public void OnBackClicked()
         {
             SceneManager.LoadScene((int)DataManager.Scenes.OverworldScene);
+        }
+
+        public void OnToPage1Clicked()
+        {
+            page1.SetActive(true);
+            page2.SetActive(false);
+        }
+
+        public void OnToPage2Clicked()
+        {
+            page1.SetActive(false);
+            page2.SetActive(true);
         }
     }
 }
