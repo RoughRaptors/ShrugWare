0e2ac3e589cabe7debfe191087e6bb8b6105f2e1
Yahel Dooley <48962645+yaheldooley@users.noreply.github.com>
2024-10-16 10:04:49 -0700

    Added some music cues and AudioClipData class

---
+++ Assets/Audio/FromYahel/AudioClipData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace ShrugWare
+{
+    [CreateAssetMenu(fileName = "NewAudioClipData", menuName = "ScriptableObjects/AudioClipData", order = 2)]
+    public class AudioClipData : ScriptableObject
+    {
+        public AudioClip clip;
+        [Range(0.1f, 1f)]
+        public float maxVolume = 1f;
+        [Range(.1f, 3f)]
+        public float pitch = 1f;
+        public bool randomPitch = false;
+        [Range(-1, 1)]
+        public float variablePitchIfRandom;
+    }
+}
