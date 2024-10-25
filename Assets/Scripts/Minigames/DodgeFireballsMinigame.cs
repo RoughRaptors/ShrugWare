cc6c80de9c930cb96668834daeecf8f5cce492ba
RoughRaptors <roughraptors@gmail.com>
2024-10-24 20:43:31 -0700

    Vfx (#253)
    * Nerfed knockback microgame. Added VFX assets. Added hit and collect VFX to first minigame
    
    * Added collectible vfx for both minigames
    
    * Added VFX and added to both minigames. I broke the heigan dance though. Lasers aren't working anymore
    
    * Removed accidental code

--- Assets/Scripts/Minigames/DodgeFireballsMinigame.cs
+++ Assets/Scripts/Minigames/DodgeFireballsMinigame.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using System.Data;
 using TMPro;
 using static UnityEngine.UIElements.UxmlAttributeDescription;
 using UnityEngine.InputSystem.EnhancedTouch;
