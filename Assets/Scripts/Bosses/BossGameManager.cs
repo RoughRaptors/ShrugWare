95040f9cc0a7a8c5ce380f6fb6e1ac15305d358a
RoughRaptors <roughraptors@gmail.com>
2024-10-25 15:40:19 -0700

    Attempted fix of heigan dance. Added 3D laser asset

--- Assets/Scripts/Bosses/BossGameManager.cs
+++ Assets/Scripts/Bosses/BossGameManager.cs
@@ -22,7 +22,7 @@ Taunt The Adds - Click x amount of adds running around the screen.
 Perform The Rotation - Click the shown abilities in the correct order
 
 -- 2.5D Character Movement --
-Avoid The Lasers - Avoid a spinning laser
+Avoid The 8Lasers - Avoid a spinning laser
 Buy Cheap/Expensive Item - Purchase the proper item at a merchant
 Colored Platforms - Get on the green platform
 Dodge The Fireball - Dodge a fireball coming at you
