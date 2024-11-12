3104e921b8c846b3a5335560685886c18c4d69e9
RoughRaptors <roughraptors@gmail.com>
2024-11-11 17:20:12 -0800

    Drag to poison heal tank (#275)
    * Added DragToPoison and HealTheTank microgames
    
    * Added documentation
    
    * Remove weird lines

--- Assets/Scripts/Microgames/Daunting Inferno/InterceptTheFireball.cs
+++ Assets/Scripts/Microgames/Daunting Inferno/InterceptTheFireball.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
-using UnityEngine.UIElements;
 
 namespace ShrugWare
 {
