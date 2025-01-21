8721b592fb29122d50b53a5dd138116c1ccf34fd
RoughRaptors <roughraptors@gmail.com>
2025-01-21 14:40:58 -0800

    More report work

--- Assets/Scripts/Microgames/Daunting Inferno/ReportChatMessage.cs
+++ Assets/Scripts/Microgames/Daunting Inferno/ReportChatMessage.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems; 
 
 namespace ShrugWare
 {
@@ -16,11 +17,15 @@ namespace ShrugWare
         [SerializeField]
         GameObject chatMessageInitialObj;
 
-        private int index = 0;
+        [SerializeField]
+        ScrollRect scrollRect;
 
+        int index = 0;
         private List<string> characterNames = new List<string> { "1", "2", "3", "4"};
 
         private List<string> chatMessages = new List<string> { "a", "b", "c", "d"};
+
+        private bool clickedCorrectMessage = false;
 
         protected override void Start()
         {
@@ -36,18 +41,26 @@ namespace ShrugWare
         {
             base.OnMyGameTick(timePercentLeft);
 
-            if(Input.GetKeyDown(KeyCode.Space))
+            scrollRect.verticalNormalizedPosition = 0;
+
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                ++index;
                 GameObject newChatMessage = Instantiate(chatMessageInitialObj, scrollViewContent.transform);
-                newChatMessage.GetComponentInChildren<TextMeshPro>().text = index.ToString();
+                newChatMessage.GetComponentInChildren<TextMeshPro>().text = index++.ToString();
+                //Resize(5, new Vector2(1, 0), ref newChatMessage);
                 newChatMessage.SetActive(true);
             }
         }
 
         protected override bool VictoryCheck()
         {
-            return true;
+            return clickedCorrectMessage;
+        }
+
+        private void Resize(float amount, Vector3 direction, ref GameObject go)
+        {
+            go.transform.position += direction * amount / 2; // Move the object in the direction of scaling, so that the corner on ther side stays in place
+            go.transform.localScale += direction * amount; // Scale object in the specified direction
         }
     }
 }
